d685c82e36a1a40f7fa2e8c82fb6526cc9a14570
Shubhangi02 <126337561+ShubhangiShahi02@users.noreply.github.com>
2024-01-03 12:47:24 +0530

    first commit

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -1,23 +1,131 @@
 
 package TicTacToe;
 
-public class TicTacToe {
+import java.util.*;
+import java.io.*;
 
-	public static void main(String[] args) {
+public class TicTacToe {
+	static HashSet<Integer> ur_set =new HashSet<Integer>();
+	static HashSet<Integer> comp_set =new HashSet<Integer>();
+	
+ 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	        char [][]g_boards={
+	        char [][]g_board={
 	            {' ','|',' ','|',' '},
 	            {'_','|','_','|','_'},
 	            {' ','|',' ','|',' '},
 	            {'_','|','_','|','_'},
 	            {' ','|',' ','|',' '}
 	        } ;
-	        print_board(g_boards);
+	        print_board(g_board);
+	        Scanner in = new Scanner(System.in);
+	        
+	       while(true) {
+	        	System.out.print("Enter value from 1-9 : ");
+	        	int user_pos=in.nextInt();
+	        	
+	        	while(ur_set.contains(user_pos)|| comp_set.contains(user_pos)) {
+	        		System.out.println();
+	        		System.out.print("Retry, Enter values from 1-9 : ");
+	        		user_pos=in.nextInt();
+	        	}
+	        	p_holder(g_board,user_pos,"You ");
+	        	
+	        	String res = check_winner();
+	        	if(res.length()>0) {
+	        		System.out.println(res);
+	        		break;
+	        	}
+	        	
+
+	        	int cpu_pos=gen_random();
+	        	
+	        	while(ur_set.contains(cpu_pos)|| comp_set.contains(cpu_pos)) {
+	        		
+	        		cpu_pos=gen_random();
+	        	}
+	        p_holder(g_board,cpu_pos,"Comp");
+	        
+	        res = check_winner();
+        	if(res.length()>0) {
+        		System.out.println(res);
+        		break;
+        	}
+	        }
 	    }
-	    static void print_board(char [][]g_boards){
-	        for(int r=0;r<g_boards[0].length; r++){
-	            for(int c=0;c<g_boards[0].length; c++){
-	                System.out.print(g_boards[r][c]);
+ 	
+ 	static String check_winner() {
+ 		HashSet<Integer> r1 = new HashSet<Integer>();
+ 		r1.add(1);
+ 		r1.add(2);
+ 		r1.add(3);
+ 		HashSet<Integer> r2 = new HashSet<Integer>();
+ 		r2.add(4);
+ 		r2.add(5);
+ 		r2.add(6);
+ 		HashSet<Integer> r3 = new HashSet<Integer>();
+ 		r3.add(7);
+ 		r3.add(8);
+ 		r3.add(9);
+ 		HashSet<Integer> c1 = new HashSet<Integer>();
+ 		c1.add(1);
+ 		c1.add(4);
+ 		c1.add(7);
+ 		HashSet<Integer> c2 = new HashSet<Integer>();
+ 		c2.add(2);
+ 		c2.add(5);
+ 		c2.add(8);
+ 		HashSet<Integer> c3 = new HashSet<Integer>();
+ 		c3.add(3);
+ 		c3.add(6);
+ 		c3.add(9);
+ 		HashSet<Integer> d1 = new HashSet<Integer>();
+ 		d1.add(1);
+ 		d1.add(5);
+ 		d1.add(9);
+ 		HashSet<Integer> d2 = new HashSet<Integer>();
+ 		d2.add(3);
+ 		d2.add(5);
+ 		d2.add(7);
+ 		
+ 		HashSet<HashSet> set = new HashSet<HashSet>();
+ 		set.add(r1);
+ 		set.add(r2);
+ 		set.add(r3);
+ 		set.add(c1);
+ 		set.add(c2);
+ 		set.add(c3);
+ 		set.add(d1);
+ 		set.add(d2);
+ 		
+ 		for(HashSet c:set) {
+ 			if(ur_set.containsAll(c))
+ 				return "You Won";
+ 			else if(comp_set.containsAll(c))
+ 				return "You lost";
+ 		}
+ 		if(ur_set.size()+comp_set.size()==9)
+ 			return "DRAW";
+ 		
+ 		return "";
+ 		
+ 		
+ 	}
+ 	
+	   static int gen_random() {
+		int max=9;
+		int min=1;
+		
+		int range = max-min+1;
+		int res = (int) (Math.random()*range) + min;
+		
+				return res;
+	}
+	    
+		static void print_board(char [][]g_board){
+	        for(int r=0;r<g_board[0].length; r++){
+	            for(int c=0;c<g_board[0].length; c++){
+	                System.out.print(g_board[r][c]);
 	            }
 	            System.out.println();
 	        }
@@ -28,11 +136,11 @@ public class TicTacToe {
 	    	char sym='X';
 	    	if(user.equals("You")) {
 	    		sym='X';
-	    		//Comment
+	    		ur_set.add(pos);
 	    	}
 	    	else if(user.equals("Comp")) {
-	    		sym='X';
-	    		//Comment
+	    		sym='0';
+	    		comp_set.add(pos);
 	    	}
 	    	switch(pos) {
 	    	case 1:
